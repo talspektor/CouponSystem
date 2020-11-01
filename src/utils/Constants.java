3432b2e3025793bf9e658758cd66a490d11d5f4e
taltalspektor <taltalspektor@localhost>
2020-11-01 23:10:02 +0200

    Feat(DBDAO

---
+++ src/utils/Constants.java
@@ -0,0 +1,20 @@
+package utils;
+
+public enum Constants {
+	DB_NAME("coupons_system"),
+	COUPONS_TABLE("coupons"),
+	COMPANIES_TABLE("compnies"),
+	CATEGORIES_TABLE("categories"),
+	CUSTOMERS_TABLE("costomers"),
+	EMAIL("email");
+	
+	private String value;
+	
+	Constants(String value) {
+		this.value = value;
+	}
+	
+	String getValue() {
+		return this.value;
+	}
+}
